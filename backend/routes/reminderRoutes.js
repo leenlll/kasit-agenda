e265bed5710b5dc27fe6160aaba9545cd3cb7ff6
leenlll <leenmohannad07@gmail.com>
2025-05-18 20:52:19 +0300

    last chance

--- backend/routes/reminderRoutes.js
+++ backend/routes/reminderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { db } = require("../firebaseAdmin"); 
+const db = require("../config/firebaseConfig");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
